b486c664eeea4acf44b47aae8429376ce517fc80
brimstonetrader <106700187+brimstonetrader@users.noreply.github.com>
2023-03-07 12:04:14 -0600

    started bringing stuff over from my version
    like 6 scripts, Different protagonist.
    
    Rn My plan is
    Level 1: They come after you
    Level 2: They move randomly
    Level 3: They avoid you
    
    Also, instead of the eating sound, I brought in a little drumroll sound effect we could use instead

---
+++ Lab4/Assets/Scripts/Level_1.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level_1 : MonoBehaviour
+{
+    public GameObject American;
+    public AudioSource audio; 
+
+    public int c = 100;
+    public string text;
+
+
+    public void OnTriggerEnter2D(Collider2D collider2D) {
+        StartCoroutine("Beep");
+        print("Entered..");
+        
+        StopCoroutine("Beep");
+    }
+    public void OnTriggerExit2D(Collider2D collider2D) {
+        StopCoroutine("Beep");
+    }
+    IEnumerator Beep()
+    {
+        if (GetComponent<Collider2D>().gameObject.CompareTag("Player")) {
+			audio.Play();
+            American.GetComponent<Renderer>().material.color = Color.red;
+            c--;
+            yield return new WaitForSeconds(0.1f);
+            audio.Pause();
+            print("ff");
+		}
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        American.transform.position = new Vector3(Random.Range(-8f, 8f),Random.Range(-6f, 6f),1);
+    }
+
+
+
+
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
